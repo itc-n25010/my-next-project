858aa84bf4ac4e76eb8c6d62e5237ab38075eebb
itc-n25010 <n25010@std.it-college.ac.jp>
2026-01-21 11:31:36 +0900

    5章まで完了

--- app/_components/Category/index.tsx
+++ app/_components/Category/index.tsx
@@ -1,9 +1,9 @@
-import type { category } from "@/app/_libs/microcms";
+import type { Category } from "@/app/_libs/microcms";
 import styles from "./index.module.css";
 import { resourceUsage } from "process";
 
 type Props = {
-  category: category;
+  category: Category;
 };
 export default function Category({ category }: Props) {
   return <span className={styles.tag}>{category.name}</span>;
